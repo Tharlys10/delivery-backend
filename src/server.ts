27e97eeaa7db8596a95782b782126665910d1ddb
tharlys10 <tharlys2015c@gmail.com>
2022-02-11 08:45:28 -0300

    feat: :sparkles: config eslint

--- src/server.ts
+++ src/server.ts
@@ -5,4 +5,4 @@ const app = express();
 
 const { APP_PORT } = process.env;
 
-app.listen(Number(APP_PORT), () => console.log(`server is running, port ${APP_PORT}`))
+app.listen(Number(APP_PORT), () => console.log(`server is running, port ${APP_PORT}`));
